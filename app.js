8f7a2e22415f3b47ffc4d753664d211835c5b159
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-04-11 16:08:35 -0400

    Minor ui changes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const burger = document.querySelector(".burger");
 const dropDown = document.querySelector(".drop-down");
+const mobileNav = document.querySelector(".mobile-nav");
 
 burger.addEventListener("click", ()=> {
     //Toggle Nav
     dropDown.classList.toggle("active");
+    mobileNav.classList.toggle("active");
     //Burger Animation
     burger.classList.toggle("toggle");
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".mobile-nav").style.top = "0";
+    } else {
+        document.querySelector(".mobile-nav").style.top = "-12vh";
+    }
+    prevScrollpos = currentScrollPos;
+    }
