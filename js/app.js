6370d45ac76ba92f933b018a5ee62045532a47b8
Steve Simkins <73185218+sdsimkins@users.noreply.github.com>
2021-05-10 13:50:06 -0400

    added updated animation for burger

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 const burger = document.querySelector(".burger");
 const dropDown = document.querySelector(".drop-down");
 const mobileNav = document.querySelector(".mobile-nav");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
 const tl = new gsap.timeline();
 
 burger.addEventListener("click", ()=> {
     animate();
     dropDown.classList.toggle("active");
     mobileNav.classList.toggle("active");
-    burger.classList.toggle("toggle");
 });
 
-tl.fromTo(dropDown, {duration: 1.5, y:"-350vw"}, {duration: 0.75, y: "0vw"});
+tl.to(line1, {duration: 0.3, rotate: 45, y: 8}, "-=1");
+tl.to(line2, {duration: 0.3, x: "-100%", opacity: 0}, "-=1");
+tl.to(line3, {duration: 0.3, rotate: -45, y: -8}, "-=1");
+tl.fromTo(dropDown, {duration: 0.75, y:"-350vw"}, {duration: 0.75, y: "0vw"}, "-=1");
 tl.reverse(-1);
 tl.reversed(true);
 
